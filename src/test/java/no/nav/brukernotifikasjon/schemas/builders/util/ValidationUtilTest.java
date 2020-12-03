e3ea311e19ccca6ab07023cf811de954ea57a412
Robert Larsen <robert.larsen@nav.no>
2020-12-03 15:17:54 +0100

    Flere tester for validationutil.

--- src/test/java/no/nav/brukernotifikasjon/schemas/builders/util/ValidationUtilTest.java
+++ src/test/java/no/nav/brukernotifikasjon/schemas/builders/util/ValidationUtilTest.java
@@ -1,9 +1,14 @@
 package no.nav.brukernotifikasjon.schemas.builders.util;
 
+import no.nav.brukernotifikasjon.schemas.builders.exception.FieldValidationException;
 import org.junit.jupiter.api.Test;
+
+import java.net.URL;
 
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.core.Is.is;
+import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 
 class ValidationUtilTest {
 
@@ -25,4 +30,43 @@ class ValidationUtilTest {
         assertThat(ValidationUtil.isPossibleFodselsnummer(alphanumeric), is(false));
     }
 
+    @Test
+    void skalKasteExceptionHvisFeltErNull() {
+        assertThrows(FieldValidationException.class, () -> ValidationUtil.validateNonNullField(null, "feltSomErNull"));
+    }
+
+    @Test
+    void skalKasteExceptionHvisTekstfeltErTomt() {
+        assertThrows(FieldValidationException.class, () -> ValidationUtil.validateNonNullField("", "feltSomErNull"));
+    }
+
+    @Test
+    void skalKasteExceptionHvisSikkerhetsnivaaErLavereEnn3() {
+        assertThrows(FieldValidationException.class, () -> ValidationUtil.validateNonNullField("", "feltSomErNull"));
+    }
+
+    @Test
+    void skalKasteExceptionHvisPaakrevdTimestampErNull() {
+        assertThrows(FieldValidationException.class, () -> ValidationUtil.localDateTimeToUtcTimestamp(null, "test", true));
+    }
+
+    @Test
+    void skalIkkeKasteExceptionHvisIkkePaakrevdTimestampErNull() {
+        assertDoesNotThrow(() -> ValidationUtil.localDateTimeToUtcTimestamp(null, "test", false));
+    }
+
+    @Test
+    void skalKasteExceptionHvisFeltErLengreEnnMaxLength() {
+        assertThrows(FieldValidationException.class, () -> ValidationUtil.validateNonNullFieldMaxLength("abc", "test", 2));
+    }
+
+    @Test
+    void skalKasteExceptionHvisLinkErLengreEnnMaxLength() {
+        assertThrows(FieldValidationException.class, () -> ValidationUtil.validateLink(new URL("https://ugyldig.url"), "testlink", 10));
+    }
+
+    @Test
+    void skalKasteExceptionHvisLinkNull() {
+        assertThrows(FieldValidationException.class, () -> ValidationUtil.validateLink(null, "testlink", 10));
+    }
 }
