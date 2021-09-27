5d4f427ebbad7959e9c36631f8586ccebf91085e
Robert Larsen <robert.larsen@nav.no>
2021-09-27 07:58:31 +0200

    Legger til felter for ekstern varsling i legacy Innboks-skjema

---
+++ src/main/java/no/nav/brukernotifikasjon/schemas/builders/InnboksBuilder.java
@@ -0,0 +1,78 @@
+package no.nav.brukernotifikasjon.schemas.builders;
+
+import no.nav.brukernotifikasjon.schemas.Innboks;
+import no.nav.brukernotifikasjon.schemas.builders.domain.Eventtype;
+import no.nav.brukernotifikasjon.schemas.builders.domain.PreferertKanal;
+import no.nav.brukernotifikasjon.schemas.builders.util.ValidationUtil;
+
+import java.net.URL;
+import java.time.LocalDateTime;
+import java.util.Arrays;
+import java.util.List;
+
+public class InnboksBuilder {
+
+    private LocalDateTime tidspunkt;
+    private String fodselsnummer;
+    private String grupperingsId;
+    private String tekst;
+    private URL link;
+    private Integer sikkerhetsnivaa;
+    private Boolean eksternVarsling = false;
+    private List<PreferertKanal> prefererteKanaler;
+
+    public InnboksBuilder withTidspunkt(LocalDateTime tidspunkt) {
+        this.tidspunkt = tidspunkt;
+        return this;
+    }
+
+    public InnboksBuilder withFodselsnummer(String fodselsnummer) {
+        this.fodselsnummer = fodselsnummer;
+        return this;
+    }
+
+    public InnboksBuilder withGrupperingsId(String grupperingsId) {
+        this.grupperingsId = grupperingsId;
+        return this;
+    }
+
+    public InnboksBuilder withTekst(String tekst) {
+        this.tekst = tekst;
+        return this;
+    }
+
+    public InnboksBuilder withLink(URL link) {
+        this.link = link;
+        return this;
+    }
+
+    public InnboksBuilder withSikkerhetsnivaa(Integer sikkerhetsnivaa) {
+        this.sikkerhetsnivaa = sikkerhetsnivaa;
+        return this;
+    }
+
+    public InnboksBuilder withEksternVarsling(Boolean eksternVarsling) {
+        this.eksternVarsling = eksternVarsling;
+        return this;
+    }
+
+    public InnboksBuilder withPrefererteKanaler(PreferertKanal... prefererteKanaler) {
+        if(prefererteKanaler != null) {
+            this.prefererteKanaler = Arrays.asList(prefererteKanaler);
+        }
+        return this;
+    }
+
+    public Innboks build() {
+        return new Innboks(
+                ValidationUtil.localDateTimeToUtcTimestamp(tidspunkt, "tidspunkt", ValidationUtil.IS_REQUIRED_TIDSPUNKT),
+                ValidationUtil.validateFodselsnummer(fodselsnummer),
+                ValidationUtil.validateNonNullFieldMaxLength(grupperingsId, "grupperingsId", ValidationUtil.MAX_LENGTH_GRUPPERINGSID),
+                ValidationUtil.validateNonNullFieldMaxLength(tekst, "tekst", ValidationUtil.MAX_LENGTH_TEXT_BESKJED),
+                ValidationUtil.validateLinkAndConvertToString(link, "link", ValidationUtil.MAX_LENGTH_LINK, ValidationUtil.isLinkRequired(Eventtype.BESKJED)),
+                ValidationUtil.validateSikkerhetsnivaa(sikkerhetsnivaa),
+                eksternVarsling,
+                ValidationUtil.validatePrefererteKanaler(eksternVarsling, prefererteKanaler)
+        );
+    }
+}
